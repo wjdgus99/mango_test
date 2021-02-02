5435f12cec6dd5a8a61a1bc5c0ca7378f9863ee2
pig <21800568@handong.edu>
2021-02-02 11:48:36 +0900

    jh: 0202 chatList.dart, friendList.dart

--- lib/chatList.dart
+++ lib/chatList.dart
@@ -4,17 +4,90 @@ class ChatList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('채팅 목록'),
-        centerTitle: true,
-        actions: [
-          IconButton(
-              icon: Icon(Icons.settings), onPressed: () => print('settings'))
-        ],
-      ),
-      body: Center(
-        child: Text('Chat List'),
-      ),
+        appBar: AppBar(
+          title: Text('채팅 목록'),
+          centerTitle: true,
+          actions: [
+            IconButton(
+                icon: Icon(Icons.settings), onPressed: () => print('settings'))
+          ],
+        ),
+        body: ListView(
+          children: ListTile.divideTiles(context: context, tiles: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ListTile(
+                leading: CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage('images/users/photo_yg.jpeg'),
+                ),
+                title: Text('박예지'),
+                subtitle: Text(
+                  '친구야 내가 레몬으로 생선회 비린내를 없애려고 하는데 이거 버릴거면 나 주라 ㅎㅎ ',
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                trailing: Text('오전 11:25'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ListTile(
+                leading: CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage('images/users/photo_si.jpeg'),
+                ),
+                title: Text('김세인'),
+                subtitle: Text(
+                  '친구야 내가 오이로 생선회 비린내를 없애려고 하는데 이거 버릴거면 나 주라',
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                trailing: Text('오전 11:20'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ListTile(
+                leading: CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage('images/users/photo_mj.jpeg'),
+                ),
+                title: Text('김민주'),
+                subtitle: Text(
+                  '인석아 밥해줘....',
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                trailing: Text('오전 11:18'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ListTile(
+                leading: CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage('images/users/photo_jh.jpeg'),
+                ),
+                title: Text('이정현'),
+                subtitle: Text(
+                  '내 모닝빵이랑 초코우유랑 혹시 바꾸지 않을래?-?',
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                trailing: Text('오전 11:12'),
+              ),
+            ),
+          ]).toList(),
+        ));
+  }
+
+  Widget _buildChatList() {
+    return ListTile(
+      leading: Image.asset('images/users/photo_yg.jpeg'),
+      title: Text('박예지'),
+      subtitle: Text('친구야....'),
+      trailing: Text('오전 11:12'),
     );
   }
 }
--- lib/friendList.dart
+++ lib/friendList.dart
@@ -1,18 +1,134 @@
 import 'package:flutter/material.dart';
 
-class FriendList extends StatelessWidget {
+import 'package:mango_test/model/exampleRefrigerator.dart';
+import 'package:mango_test/model/exampleUserList.dart';
+import 'model/food.dart';
+
+import 'package:mango_test/model/exampleUser.dart';
+import 'model/user.dart';
+
+import 'package:top_sheet/top_sheet.dart';
+
+class FriendList extends StatefulWidget {
+  @override
+  _FriendListState createState() => _FriendListState();
+}
+
+class _FriendListState extends State<FriendList> {
+  User user = localUser.loadUser();
+  List<User> friendList = localUserList.loadUserList();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('친구 목록'),
+        title: Text('친구'),
         centerTitle: true,
         actions: [
+          IconButton(
+              icon: Icon(Icons.person_add_alt), onPressed: () => _topSheet()),
           IconButton(
               icon: Icon(Icons.settings), onPressed: () => print('settings'))
         ],
       ),
-      body: ListTile(trailing: Text('Friend List')),
+      body: ListView(children: [
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              radius: 30,
+              backgroundImage: AssetImage(user.Image),
+            ),
+            title: Text(user.Name),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              radius: 30,
+              backgroundImage: AssetImage(user.Image),
+            ),
+            title: Text(user.FriendList.toString()),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              radius: 30,
+              backgroundImage: AssetImage(user.Image),
+            ),
+            title: Text(friendList.length.toString()),
+          ),
+        ),
+      ]),
+    );
+  }
+
+  Widget _buildFriendList() {
+    return ListTile(
+        leading: Image.asset(user.Image),
+        title: Text(friendList.length.toString()));
+  }
+
+  Widget _topSheet() {
+    TopSheet.show(
+      context: context,
+      child: Column(children: [
+        ListTile(
+          // leading: IconButton(icon: Icon(Icons.cancel)),
+          title: Row(children: [
+            Icon(Icons.cancel),
+            Expanded(child: Center(child: Text('친구 추가')))
+          ]),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Column(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.qr_code_scanner),
+                    onPressed: () => print('QR 코드'),
+                  ),
+                  Text('QR 코드')
+                ],
+              ),
+              Column(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.contact_phone),
+                    onPressed: () => print('연락처로 추가'),
+                  ),
+                  Text('연락처로 추가')
+                ],
+              ),
+              Column(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.add_circle_outline),
+                    onPressed: () => print('ID로 추가'),
+                  ),
+                  Text('ID로 추가')
+                ],
+              ),
+              Column(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.person_add_alt),
+                    onPressed: () => print('추천친구'),
+                  ),
+                  Text('추천친구')
+                ],
+              ),
+            ],
+          ),
+        ),
+      ]),
+      direction: TopSheetDirection.TOP,
     );
   }
 }
