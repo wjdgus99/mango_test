e2d833eaab99d8e9db1ba8e139c66a9bbfe45fc7
Kimminju98 <21700097@handong.edu>
2021-02-04 09:52:28 +0900

    mj - 02.04(목) morning

--- lib/itemCreate.dart
+++ lib/itemCreate.dart
@@ -41,39 +41,43 @@ class _ItemCreateState extends State<ItemCreate> {
         title: Text(appTitle),
       ),
       body: ListView(
+        padding: EdgeInsets.all(DeviceWidth * 0.05),// mj: TODO. DeviceWidth로 바꾸기
         children: <Widget>[
           showInfo(),
+          Expanded(child: Container()),
         ],
+      ),
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.all(DeviceWidth * 0.05),
+        child: Container(
+          height: 70,
+          child: FlatButton(
+            color: Theme.of(context).accentColor,
+            child: Text('등록'),
+            onPressed: (){},
+          ),
+        ),
       ),
     );
   }
 
   Widget showInfo() {
     return Container(
-      padding: EdgeInsets.all(20), // mj: TODO. DeviceWidth로 바꾸기
       child: Column(
         children: <Widget>[
           Container(alignment: Alignment.topLeft, child: Text('제품정보')),
-          TextFormField(
-            controller: _nameTextController,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              prefixIcon:  Text('품명'),
-              prefixIconConstraints: BoxConstraints(minWidth: 0, minHeight: 0),
-              suffixIcon: IconButton(
-                icon: Icon(Icons.clear),
-                onPressed: () {},
-              ),
-            ),
-          ),
+          showTextField(_nameTextController, '품명'),
           Row(
-            children: <Widget>[],
+            children: <Widget>[
+              showSelectField('수량'),
+              showSelectField('카테고리'),
+            ],
           ),
           Container(alignment: Alignment.topLeft, child: Text('보관방법')),
           Container(
-            width: 500, // mj: TODO. DeviceWidth
+            width: DeviceWidth, // mj: TODO. DeviceWidth
             child: CupertinoSegmentedControl(
-              padding: EdgeInsets.all(10),
+              //padding: EdgeInsets.all(5), // mj: TODO. DeviceWidth
               children: tabBarWidget,
               onValueChanged: (int val) {
                 setState(() {
@@ -108,8 +112,58 @@ class _ItemCreateState extends State<ItemCreate> {
               Text('구매일'),
             ],
           ),
-          TextField(),
+          showSelectField('구매일'),
         ],
+      ),
+    );
+  }
+
+  Widget showTextField(TextEditingController myController, String info){
+    return TextFormField(
+      controller: myController,
+      decoration: InputDecoration(
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        border: OutlineInputBorder(),
+        prefixIcon: Padding(
+          padding: const EdgeInsetsDirectional.fromSTEB(20, 0, 10, 0),
+          child: Text(info), // myIcon is a 48px-wide widget.
+        ),
+        prefixIconConstraints: BoxConstraints(minWidth: 0, minHeight: 0),
+        suffixIcon: IconButton(
+          icon: Icon(Icons.clear),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+
+  Widget showSelectField(String info){
+    return Container(
+      height: 60,
+      decoration: BoxDecoration(
+        border: Border.all(
+            color: Colors.grey
+        ),
+        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+      ),
+      child: Center(
+        child: Row(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsetsDirectional.fromSTEB(20, 0, 20, 0),
+              child: Text(info), // myIcon is a 48px-wide widget.
+            ),
+            Text('3'),
+            IconButton(
+              icon: Icon(Icons.arrow_drop_down),
+              onPressed: (){},
+            ),
+            // Padding(
+            //   padding: const EdgeInsetsDirectional.fromSTEB(20, 0, 10, 0),
+            //   child: Icon(Icons.arrow_drop_down), // myIcon is a 48px-wide widget.
+            // ),
+          ],
+        ),
       ),
     );
   }
