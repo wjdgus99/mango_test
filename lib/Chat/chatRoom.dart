f1b8247e5af017dd89f9e5ac2b72e4ae7031dc15
pig <21800568@handong.edu>
2021-02-17 18:18:15 +0900

    jh: 0217 chat

--- lib/Chat/chatRoom.dart
+++ lib/Chat/chatRoom.dart
@@ -13,8 +13,13 @@ class _ChatRoomState extends State<ChatRoom> {
   bool _isComposing = false;
   final FocusNode _focusNode = FocusNode();
 
+  bool me = true;
+
   @override
   Widget build(BuildContext context) {
+    me = false;
+    // me = true;
+
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -24,7 +29,7 @@ class _ChatRoomState extends State<ChatRoom> {
             Navigator.pop(context);
           },
         ),
-        title: Text('user1'),
+        title: Text('민주'),
         centerTitle: true,
       ),
       body: Container(
@@ -33,23 +38,15 @@ class _ChatRoomState extends State<ChatRoom> {
             Flexible(
                 child: ListView(
               children: [
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: CircleAvatar(
-                        radius: 20,
-                        backgroundImage:
-                            AssetImage('images/users/photo_jh.jpeg'),
-                        backgroundColor: Colors.white60,
-                      ),
-                    ),
-                    Expanded(
-                        child: SizedBox(
-                      height: 20,
-                    ))
-                  ],
-                )
+                _buildTextMSG(false, '민주', '오이 좀 나눠 주세요..ㅎㅎ '),
+                _buildTextMSG(true, '정현', '싫은데요 '),
+                _buildTextMSG(true, '정현', '뭐 맛있는거 해먹을라고 ?-?'),
+                _buildTextMSG(false, '민주', '오이 좀 나눠 주세요..ㅎㅎ '),
+                _buildTextMSG(false, '민주', '오이 좀 나눠 주세요..ㅎㅎ '),
+                _buildTextMSG(false, '민주', '오이 좀 나눠 주세요..ㅎㅎ '),
+                _buildTextMSG(true, '정현', '싫은데요 '),
+                _buildTextMSG(true, '정현', '싫은데요 '),
+                _buildTextMSG(true, '정현', '싫어싫어싫어싫어싫어싫어싫어싫어싫\n어싫어싫어싫어싫어싫어싫다고!! '),
               ],
             )),
             Divider(height: 1.0),
@@ -105,7 +102,9 @@ class _ChatRoomState extends State<ChatRoom> {
                 margin: EdgeInsets.symmetric(horizontal: 4.0),
                 child: IconButton(
                   icon: Icon(Icons.send),
-                  color: _isComposing ? Colors.red : Colors.grey,
+                  color: _isComposing
+                      ? Theme.of(context).accentColor
+                      : Colors.grey,
                   onPressed: _isComposing
                       ? () => _handleSubmitted(_textController.text)
                       : null,
@@ -113,6 +112,56 @@ class _ChatRoomState extends State<ChatRoom> {
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildTextMSG(bool me, String sender, String text) {
+    return Material(
+      child: Row(
+          mainAxisAlignment:
+              me ? MainAxisAlignment.end : MainAxisAlignment.start,
+          children: [
+            !me
+                ? CircleAvatar(
+                    radius: 20,
+                    backgroundImage: AssetImage('images/users/photo_mj.jpeg'),
+                    backgroundColor: Colors.white60,
+                  )
+                : SizedBox(),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                  crossAxisAlignment:
+                      me ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(sender),
+                    Container(
+                      margin: me
+                          ? EdgeInsets.only(left: 30)
+                          : EdgeInsets.only(right: 30),
+                      child: Material(
+                        color: me
+                            ? Theme.of(context).accentColor
+                            : Colors.grey[300],
+                        borderRadius: BorderRadius.circular(10.0),
+                        elevation: 6.0,
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 10.0, horizontal: 10.0),
+                          child: Text(text),
+                        ),
+                      ),
+                    ),
+                  ]),
+            ),
+            me
+                ? CircleAvatar(
+                    radius: 20,
+                    backgroundImage: AssetImage('images/users/photo_jh.jpeg'),
+                    backgroundColor: Colors.white60,
+                  )
+                : SizedBox(),
+          ]),
     );
   }
 }
